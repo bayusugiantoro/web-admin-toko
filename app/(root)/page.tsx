e380bec0eb2a4ee284a0a6c4671d9d4739e2f767
Bayu Sugiantoro <bayusugiantoro5@gmail.com>
2024-09-13 22:05:44 +0700

    setup clerk authentication

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,11 @@
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <>
       <div className="p-4">
-        <Button size="lg" variant="destructive">
-          Click Me
-        </Button>
+        <UserButton afterSignOutUrl="/" />
       </div>
     </>
   );
