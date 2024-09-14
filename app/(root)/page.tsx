01d81d533df6fb63606152b1f7f1a67d8abd7df9
Bayu Sugiantoro <bayusugiantoro5@gmail.com>
2024-09-14 21:42:59 +0700

    test dialog shadcn

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,21 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
+import Modal from "@/components/ui/modal";
 import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <>
       <div className="p-4">
-        <UserButton afterSignOutUrl="/" />
+        <Modal
+          title="Test Title"
+          description="test description"
+          isOpen
+          onClose={() => {}}
+        >
+          Children
+        </Modal>
       </div>
     </>
   );
